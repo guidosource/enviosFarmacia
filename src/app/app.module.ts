3ccbcf2463ee31524a2460916703167aefcf6c5b
guido <guido.vespignani@gmail.com>
2018-05-11 00:24:09 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AltaclienteComponent } from './components/altacliente/altacliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AltaclienteComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/altacliente/altacliente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-altacliente',
+  templateUrl: './altacliente.component.html',
+  styleUrls: ['./altacliente.component.css']
+})
+export class AltaclienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
